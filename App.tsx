5c8ac970ab3edd0496f31831b2fb03ab11a673c6
Barbosa <daniel.barbosa@capgemini.com>
2023-05-18 15:33:30 -0300

    Configurando notificacao no app

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
+
 import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +10,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('77b72667-9385-44d4-95ed-ae893db224f5');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
